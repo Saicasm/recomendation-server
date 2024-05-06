fa2208aa29a8fce55d98a07a807a8f0a88ad45a3
Sai Kumar N <sai98nsk@gmail.com>
2024-05-05 20:16:36 -0400

    style: code format

--- recom-core/src/main/java/com/recommendation/core/search/SearchService.java
+++ recom-core/src/main/java/com/recommendation/core/search/SearchService.java
@@ -1,19 +1,21 @@
 package com.recommendation.core.search;
 
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
 public class SearchService {
     private final SearchRepository searchRepository;
+
     @Autowired
     public SearchService(SearchRepository searchRepository) {
         this.searchRepository = searchRepository;
     }
+
     public List<Search> search(String query) {
         if (query.isEmpty()) {
             return searchRepository.list();
